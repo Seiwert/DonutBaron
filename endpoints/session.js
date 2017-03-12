8ecbbb354e2e0cfa034aa8c424fff808d819bc72
Seiwert <adamseiwert@gmail.com>
2017-03-12 17:07:54 -0600

    Passwords are now hashed!

--- endpoints/session.js
+++ endpoints/session.js
@@ -1,7 +1,8 @@
 "use strict"
 
 var db = require('../db'),
-bcrypt = require('bcrypt-nodejs');
+bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 // The Session endpoint is in charge of creating user sessions, and rendering the login/register pages.
 class Session {
@@ -55,46 +56,48 @@ class Session {
         message: "Not applicable organization. Ability to create other organizations may be available in the future."
       })
     }
-    bcrypt.hash(req.body.password, null, null, function(err,hash) {
-      if (err) {
-        console.log("Unable to hash password",err)
-        return res.sendStatus(500)
-      }
-      db.run("INSERT INTO users (username_text, real_name, organization, email_address, phone_number, password, donut_quality_rating, donut_reliability_rating, has_rated_this_week, is_donut_baron, is_admin) values (?,?,?,?,?,?,?,?,?,?,?)",
-      req.body.Username,
-      req.body.first_name + " " + req.body.last_name,
-      req.body.organization_name,
-      req.body.email,
-      req.body.phone,
-      hash,
-      0,
-      0,
-      0,
-      0,
-      0,
-      (err, user) => {
-        // Send an error if the user can't be registered.
-        if (err) return res.render('register', {
-          message: "Error attempting to register. Please try again."
-        });
-        // Create a session for the newly registered user.
-        db.get("SELECT * FROM users WHERE username_text = ?", req.body.Username, (err, user) => {
-          req.session.user_id = user.userID;
-          req.user = user;
-          db.get("SELECT * FROM upcomingList WHERE userID = ?", -1, function(err, nextUserSlot) {
-            if (err) return res.sendStatus(500);
-            // Add them to the next avaialable space in the database upcomingList.
-            db.run("UPDATE upcomingList SET userID= ?, real_name = ? WHERE listID = ?", user.userID, user.real_name, nextUserSlot.listID, function(err) {
-              if (err) {
-                console.log("If there are already 30 on the donut list, adding another becomes impossible.");
-                return res.sendStatus(500);
-              }
-            });
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+      bcrypt.hash(req.body.Password, salt, function(err,hash) {
+        if (err) {
+          console.log("Unable to hash password",err)
+          return res.sendStatus(500)
+        }
+        db.run("INSERT INTO users (username_text, real_name, organization, email_address, phone_number, password, donut_quality_rating, donut_reliability_rating, has_rated_this_week, is_donut_baron, is_admin) values (?,?,?,?,?,?,?,?,?,?,?)",
+        req.body.Username,
+        req.body.first_name + " " + req.body.last_name,
+        req.body.organization_name,
+        req.body.email,
+        req.body.phone,
+        hash,
+        0,
+        0,
+        0,
+        0,
+        0,
+        (err, user) => {
+          // Send an error if the user can't be registered.
+          if (err) return res.render('register', {
+            message: "Error attempting to register. Please try again."
           });
-          return res.redirect('/');
+          // Create a session for the newly registered user.
+          db.get("SELECT * FROM users WHERE username_text = ?", req.body.Username, (err, user) => {
+            req.session.user_id = user.userID;
+            req.user = user;
+            db.get("SELECT * FROM upcomingList WHERE userID = ?", -1, function(err, nextUserSlot) {
+              if (err) return res.sendStatus(500);
+              // Add them to the next avaialable space in the database upcomingList.
+              db.run("UPDATE upcomingList SET userID= ?, real_name = ? WHERE listID = ?", user.userID, user.real_name, nextUserSlot.listID, function(err) {
+                if (err) {
+                  console.log("If there are already 30 on the donut list, adding another becomes impossible.");
+                  return res.sendStatus(500);
+                }
+              });
+            });
+            return res.redirect('/');
+          });
         });
       });
-    })
+    });
   }
 
   // Ends a user session by flushing the session cookie. Renders the login page, and passes through an empty message.
